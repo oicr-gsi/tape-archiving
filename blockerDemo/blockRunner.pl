49aa9889b1b795cc692b1ba93a41377630dece6d
aduncan <andrew.duncan@oicr.on.ca>
2015-05-13 09:20:33 -0400

    Removed some unnecessary comments

--- blockerDemo/blockRunner.pl
+++ blockerDemo/blockRunner.pl
@@ -90,12 +90,6 @@ while (<$INDEXFILE_FH>)  #run through instructions file
 		my (undef, $Path, $Size)	=	split (/\t/,$_);
 		chomp ($Path);	#remove the newline from the path
 		
-		# Andrew Code Addition
-		# print out path and file size to std out (redirected to a file)
-		# $Size should represent the current lines related file size
-		print "'$Path' is of size '$Size'\n";		 
-
-
 		#Reset to the start of the line:			
 #		print "D: File Pointer set to: ", tell ($INDEXFILE_FH),"\n";
 		my $BackupLength= length ($_);
